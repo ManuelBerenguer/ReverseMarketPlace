6a13964cdf561399f41b49147001a59ddbd331b2
Manuel Berenguer <manuel.berenguer@elcome.ltd.uk>
2019-12-17 17:52:44 +0100

    New DemandsController in Demands API with action to post a new demand

---
+++ ReverseMarketPlace.Demands.API/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReverseMarketPlace.Common.Dispatchers;
+using ReverseMarketPlace.Common.Types.Messages;
+using ReverseMarketPlace.Common.Types.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseMarketPlace.Demands.API.Controllers
+{
+    /// <summary>
+    /// Shared things across all Controllers
+    /// </summary>
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        /// <summary>
+        /// Dispatcher to dispatch commands or queries (CQRS)
+        /// </summary>
+        private readonly IDispatcher _dispatcher;
+
+        public BaseController(IDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        /// <summary>
+        /// Method to dispatch a command (CQRS write side)
+        /// </summary>
+        /// <param name="command">The command to be dispatched</param>
+        /// <returns>Task</returns>
+        protected async Task SendAsync(ICommand command)
+            => await _dispatcher.SendAsync(command);
+
+        /// <summary>
+        /// Method to dispatch a query (CQRS read side)
+        /// </summary>
+        /// <typeparam name="TResult">Generic result type</typeparam>
+        /// <param name="query">The query to be dispatched</param>
+        /// <returns>Task of TResult</returns>
+        protected async Task<TResult> QueryAsync<TResult>(IQuery<TResult> query)
+            => await _dispatcher.QueryAsync<TResult>(query);
+    }
+}
---
+++ ReverseMarketPlace.Demands.API/Controllers/DemandsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReverseMarketPlace.Common.Dispatchers;
+using ReverseMarketPlace.Demands.Core.Messages.Commands.Demands;
+
+namespace ReverseMarketPlace.Demands.API.Controllers
+{
+    [Route("api/[controller]")]    
+    public class DemandsController : BaseController
+    {
+        public DemandsController(IDispatcher dispatcher) : base(dispatcher)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateDemand createDemandCommand)
+        {
+            await SendAsync(createDemandCommand);
+
+            return AcceptedAtAction(nameof(Post));
+        }
+
+    }
+}
